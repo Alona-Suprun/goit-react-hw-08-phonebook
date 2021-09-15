b8e945568363e3717ac638fb48e3f7053eb0f93c
Alona-Suprun <a.alonka.l@gmail.com>
2021-09-15 17:23:41 +0300

    add home page

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,10 @@
+const HomePage = () => {
+  return (
+    <>
+      <h1>People are the most important, unique, and precious resource.</h1>
+      <p>Be in touch and use our conveniont phonebook app.</p>
+    </>
+  );
+};
+
+export default HomePage;
